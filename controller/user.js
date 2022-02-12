24662eb0697f353043a6c2968a5c37b29323d72c
toth2000 <tothagata.bhattacharjee@gmail.com>
2022-02-13 01:22:35 +0530

    Add find users

--- controller/user.js
+++ controller/user.js
@@ -81,7 +81,63 @@ const updateUserLastDonationDate = async (req, res) => {
 };
 
 /** Find list of nearby users within the range */
-const findUsers = async (req, res) => {};
+const findUsers = async (req, res) => {
+  try {
+    const { lat, lon, range } = req.query;
+
+    if (!lat || !lon || !range)
+      return res
+        .status(401)
+        .json({ message: "Query data not in corrent format" });
+
+    const deg2rad = (deg) => {
+      return deg * (Math.PI / 180);
+    };
+
+    const getDistance = (lat1, lon1, lat2, lon2) => {
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2 - lat1); // deg2rad below
+      var dLon = deg2rad(lon2 - lon1);
+      var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) *
+          Math.cos(deg2rad(lat2)) *
+          Math.sin(dLon / 2) *
+          Math.sin(dLon / 2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      var d = R * c; // Distance in km
+      return d;
+    };
+
+    const comparator = (a, b) => {
+      const dist1 = getDistance(lat, lon, a.lat, a.lon);
+      const dist2 = getDistance(lat, lon, b.lat, b.lon);
+      if (dist1 < dist2) return -1;
+      if (dist1 > dist2) return 1;
+      return 0;
+    };
+
+    const users = await User.find();
+
+    const allUsers = users.filter((element) => {
+      const dist = getDistance(
+        lat,
+        lon,
+        element.location.latitude,
+        element.location.longitude
+      );
+
+      if (dist > range) return false;
+      else return true;
+    });
+
+    allUsers.sort(comparator);
+    res.status(200).json(allUsers);
+  } catch (error) {
+    console.log("user controller getUser", error);
+    return res.status(500).json({ message: "Server error", error: error });
+  }
+};
 
 module.exports = {
   storeUser,
